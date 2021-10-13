6103b107a30df5fa7fbc2bef41e7c86df9a1f6f2
Fulvio <manfrin.fulvio@gmail.com>
2021-10-13 14:14:53 -0300

    refactoring for organization

--- Librarian/Librarian.InfraData/Repository/Generic/GenericRepository.cs
+++ Librarian/Librarian.InfraData/Repository/Generic/GenericRepository.cs
@@ -1,101 +1,94 @@
-﻿using Domain.Models;
-using Domain.Models.Base;
+﻿using Domain.Models.Base;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InfraData.Repository.Generic
 {
     public class GenericRepository<T>: IRepository<T> where T : BaseEntity
     { 
 
-    private MySQLContext _context;
+        private MySQLContext _context;
 
-    private DbSet<T> dataset;
+        private DbSet<T> dataset;
 
-    public GenericRepository(MySQLContext context)
-    {
-        _context = context;
-        dataset = _context.Set<T>();
-    }
-
-    // Method responsible for returning all Books,
-    public List<T> FindAll()
-    {
-        return dataset.ToList();
-    }
-
-    public T FindById(long id)
-    {
-        return dataset.SingleOrDefault(p => p.Id.Equals(id));
-    }
-
-
-    public T Create(T item)
-    {
-        try
+        public GenericRepository(MySQLContext context)
         {
-            dataset.Add(item);
-            _context.SaveChanges();
+            _context = context;
+            dataset = _context.Set<T>();
         }
-        catch (Exception)
+    
+        public List<T> FindAll()
         {
-            throw;
+            return dataset.ToList();
         }
-        return item;
-    }
 
+        public T FindById(long id)
+        {
+            return dataset.SingleOrDefault(p => p.Id.Equals(id));
+        }
 
-    public T Update(T item)
-    {
-
-        if (!Exists(item.Id)) return null;
-
-        // Get the current status of the record in the database
-        var result = dataset.SingleOrDefault(p => p.Id.Equals(item.Id));
-        if (result != null)
+        public T Create(T item)
         {
             try
             {
-                // set changes and save
-                _context.Entry(result).CurrentValues.SetValues(item);
-                _context.SaveChanges();
-                return result;
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-        else
-        {
-            return null;
-        }
-    }
-
-    // Method responsible for deleting a book from an ID
-    public void Delete(long id)
-    {
-        var result = dataset.SingleOrDefault(p => p.Id.Equals(id));
-        if (result != null)
-        {
-            try
-            {
-                dataset.Remove(result);
+                dataset.Add(item);
                 _context.SaveChanges();
             }
             catch (Exception)
             {
                 throw;
             }
+            return item;
+        }
+
+        public T Update(T item)
+        {
+
+            if (!Exists(item.Id)) return null;
+
+            // Get the current status of the record in the database
+            var result = dataset.SingleOrDefault(p => p.Id.Equals(item.Id));
+            if (result != null)
+            {
+                try
+                {
+                    // set changes and save
+                    _context.Entry(result).CurrentValues.SetValues(item);
+                    _context.SaveChanges();
+                    return result;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    
+        public void Delete(long id)
+        {
+            var result = dataset.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    dataset.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+        public bool Exists(long id)
+        {
+            return dataset.Any(p => p.Id.Equals(id));
         }
     }
-    public bool Exists(long id)
-    {
-        return dataset.Any(p => p.Id.Equals(id));
-    }
-}
 }
